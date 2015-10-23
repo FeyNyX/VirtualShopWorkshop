9f3107ae1f5f284be0800cb3462b0b534bae521a
Wojtek <wojtek.lenartowicz@gmail.com>
2015-10-23 16:37:48 +0200

    registration improvements

---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+?>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+            <?php
+            if(!$_SESSION['isLoggedIn']){
+                echo('
+                <form class="navbar-form navbar-left" role="form" action="" method="post">
+                <div class="form-group">
+                    <input class="form-control" id="login" type="text" placeholder="Login">
+                </div>
+                <div class="form-group">
+                    <input class="form-control" id="password" type="text" placeholder="Password">
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon" id="basic-addon2"><i class="fa fa-sign-in"></i></span>
+                    <input type="submit" class="form-control" value="Log in" aria-describedby="basic-addon2">
+                </div>
+            </form>
+            ');
+            }
+            ?>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#band">BAND</a></li>
+                <li><a href="#tour">TOUR</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">MORE
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Merchandise</a></li>
+                        <li><a href="#">Extras</a></li>
+                        <li><a href="#">Media</a></li>
+                    </ul>
+                </li>
+                <li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+require_once("api/api.php");
 ?>
 
 <!DOCTYPE html>
@@ -6,40 +7,20 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>
+    <style type="text/css">
+        body {
+            background: #f5f8fa !important;
+            font-family: 'Raleway', sans-serif !important;
+        }
+    </style>
     <title></title>
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Logo</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#home">HOME</a></li>
-                <li><a href="#band">BAND</a></li>
-                <li><a href="#tour">TOUR</a></li>
-                <li><a href="#contact">CONTACT</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">MORE
-                        <span class="caret"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Merchandise</a></li>
-                        <li><a href="#">Extras</a></li>
-                        <li><a href="#">Media</a></li>
-                    </ul>
-                </li>
-                <li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php
+include('header.php');
+?>
 <div class="row">
     <div class="col-sm-4"></div>
     <div class="col-sm-4">
@@ -47,44 +28,38 @@
         <h2>Register:</h2>
         <form class="form-horizontal" role="form">
             <div class="form-group">
-                <label class="control-label col-sm-2" for="name">Name:</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="name" placeholder="Enter name">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="surname">Surname:</label>
                 <div class="col-sm-10">
                     <input type="text" class="form-control" id="surname" placeholder="Enter surname">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="address">Address:</label>
                 <div class="col-sm-10">
                     <input type="address" class="form-control" id="address" placeholder="Enter address">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="email">Email:</label>
                 <div class="col-sm-10">
                     <input type="email" class="form-control" id="email" placeholder="Enter email">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd">Password:</label>
                 <div class="col-sm-10">
                     <input type="password" class="form-control" id="pwd" placeholder="Enter password">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd2">Repeat password:</label>
                 <div class="col-sm-10">
                     <input type="password" class="form-control" id="pwd2" placeholder="Repeat password">
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-default">Submit</button>
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-info btn-reg">Register</button>
                 </div>
             </div>
         </form>
